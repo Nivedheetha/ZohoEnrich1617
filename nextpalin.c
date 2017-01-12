856c7c61a88fa49be72b76173a2b06fb2cd2f334
Nivedheetha <nivedheetha@gmail.com>
2017-01-12 17:45:59 +0530

    Create nextpalin.c

---
+++ nextpalin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void) 
+{
+	// your code goes here
+	int num,original,rev,rem,next,palin=0;
+	printf("enter the number:");
+	scanf("%d",&num);
+	next=num+1;
+	original=next;
+	do
+	{
+	    rev=0;
+	    next=original;
+	    while(next>0)
+	    {
+	        rem=next%10;
+		    rev=rev*10+rem;
+		    next=next/10;
+	    }
+	    if(original==rev)
+	    {   palin=1;
+		    printf("%d",original);
+	    }
+	    original++;
+	}while(palin!=1);
+	return 0;
+}
