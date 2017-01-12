e7b1c4deb221953e0dcc9cf553ab888f72a93546
Nivedheetha <nivedheetha@gmail.com>
2017-01-12 16:52:54 +0530

    Create nextprime.c

---
+++ nextprime.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ int main(void) 
+ {
+	// your code goes here
+	int i,n,flag,prime=0,n1;
+	printf("enter the number");
+	scanf("%d",&n);
+	n1=n+1;
+	do
+    {
+	    flag=0;
+	    for(i=2;i<=n1/2;i++)
+	    {
+		    if(n1%i==0)
+		    {
+			    flag=1;
+			    break;
+		    }
+	    }
+	    if(flag==0)
+	    {
+		    printf("%d",n1);
+		    prime=1;
+	    }
+	    n1++;
+	}while(prime!=1);
+	return 0;
+}
