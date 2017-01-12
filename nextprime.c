71cd359f8944413390dc73052231e2b8cd9f8cd3
Nivedheetha <nivedheetha@gmail.com>
2017-01-12 17:59:24 +0530

    Update nextprime.c

--- nextprime.c
+++ nextprime.c
@@ -7,22 +7,22 @@
 	scanf("%d",&n);
 	n1=n+1;
 	do
-    {
-	    flag=0;
-	    for(i=2;i<=n1/2;i++)
-	    {
-		    if(n1%i==0)
-		    {
-			    flag=1;
-			    break;
-		    }
-	    }
-	    if(flag==0)
-	    {
-		    printf("%d",n1);
-		    prime=1;
-	    }
-	    n1++;
+	{
+		flag=0;
+		for(i=2;i<=n1/2;i++)
+		{
+			if(n1%i==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			printf("%d",n1);
+			prime=1;
+		}
+		n1++;
 	}while(prime!=1);
 	return 0;
 }
