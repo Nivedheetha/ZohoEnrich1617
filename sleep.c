39c2cce3c5fefe02795f83014845743b6fa4d33d
Nivedheetha <nivedheetha@gmail.com>
2017-01-14 15:11:28 +0530

    Update sleep.c

--- sleep.c
+++ sleep.c
@@ -7,9 +7,9 @@ int main(void)
 	char string[]={'H','A','P','P','Y',' ','P','O','N','G','A','L','\0'};
 	for(i=0;string[i]!='\0';i++)
 	{
-	    printf("%c",string[i]);
-	    fflush(stdout);
-	    usleep(10*1000);
+		printf("%c",string[i]);
+		fflush(stdout);
+		usleep(10*1000);
 	}
 	return 0;
 }
