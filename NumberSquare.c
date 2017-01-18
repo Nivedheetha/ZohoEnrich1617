ebb0580cf02f8ab8238111832cfca16317a6d5fd
Nivedheetha <nivedheetha@gmail.com>
2017-01-18 19:40:57 +0530

    Update NumberSquare.c

--- NumberSquare.c
+++ NumberSquare.c
@@ -3,80 +3,80 @@
 #include<conio.h>
 void display(int matrix[4][4])
 {
-  int i,j;
-  for(i=0;i<4;i++)
-  {
-    printf("\n-------------\n|");
-    for(j=0;j<4;j++)
-    {   
-      if(matrix[i][j]==32)
-      {
-        printf("%c |",matrix[i][j]);
-      }
-      else if(matrix[i][j]>9)
-      {
-        printf("%d|",matrix[i][j]);
-      }
-      else
-      {
-        printf("0%d|",matrix[i][j]);
-      }
-    }
-  }
-  printf("\n-------------\n");
+	int i,j;
+	for(i=0;i<4;i++)
+	{
+		printf("\n-------------\n|");
+		for(j=0;j<4;j++)
+		{
+			if(matrix[i][j]==32)
+			{
+				printf("%c |",matrix[i][j]);
+			}
+			else if(matrix[i][j]>9)
+			{
+				printf("%d|",matrix[i][j]);
+			}
+			else
+			{
+				printf("0%d|",matrix[i][j]);
+			}
+		}
+	}
+	printf("\n-------------\n");
 }
 int main()
 {
-  int matrix[4][4]={2,1,4,3,6,5,32,7,9,8,11,10,13,12,15,14};
+	int matrix[4][4]={2,1,4,3,6,5,32,7,9,8,11,10,13,12,15,14};
 	char choice;
 	int tempi,tempj;
-	int i=1,j=2,k,m;
+	int i=1,j=2;
 	clrscr();
 	while(1)
 	{
-	  display(matrix);
-	  printf("\nenter your choice: u,d,l,r,q\n");
-	  scanf("%c",&choice);
-	  switch(choice)
-	  {
-	    case 'l':
-                tempi=i;
-	              if(j==0)
-	              {
-	                tempj=j+4-1;
-	              }
-	              else
-	              {
-	                tempj=j-1;
-	              }
-	              break;
-	    case 'r':
-	              tempi=i;
-	              tempj=(j+1)%4;
-	              break;
-	    case 'u':
-	              if(i==0)
-	              {
-	                tempi=i+4-1;
-	              }
-	              else
-	              {
-	                tempi=i-1;
-	              }
-	              tempj=j;
-	              break;
-	    case 'd':
-	              tempi=(i+1)%4;
-	              tempj=j;
-	              break;
-      case 'q':
-                exit(0);
-    }
-	  matrix[i][j]=matrix[tempi][tempj];
-	  matrix[tempi][tempj]=32;
-	  i=tempi;
-	  j=tempj;
-	  clrscr();
+		display(matrix);
+		printf("\npress u-->up d-->down l-->left r-->right q-->quit\nenter a character\n");
+		scanf("%c",&choice);
+		switch(choice)
+		{
+			case 'l':
+				tempi=i;
+				if(j==0)
+				{
+					tempj=j+4-1;
+				}
+				else
+				{
+					tempj=j-1;
+				}
+				break;
+			case 'r':
+				tempi=i;
+				tempj=(j+1)%4;
+				break;
+			case 'u':
+				if(i==0)
+				{
+					tempi=i+4-1;
+				}
+				else
+				{
+					tempi=i-1;
+				}
+				tempj=j;
+				break;
+			case 'd':
+				tempi=(i+1)%4;
+				tempj=j;
+				break;
+			case 'q':
+				exit(0);
+		}
+		matrix[i][j]=matrix[tempi][tempj];
+		matrix[tempi][tempj]=32;
+		i=tempi;
+		j=tempj;
+		clrscr();
 	}
-  return 0;
+	return 0;
 }
