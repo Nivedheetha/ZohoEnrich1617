1e7104603afb286a93187007d35c13f1e0e1137b
Nivedheetha <nivedheetha@gmail.com>
2017-01-21 15:24:34 +0530

    Update Subset.c

--- Subset.c
+++ Subset.c
@@ -2,18 +2,19 @@
 #include<math.h>
 int main(void) 
 {
-	int n,n1,set[5],j,a[32][5],num,rem,i;
+	int n,n1,j,num,rem,i;
 	printf("enter size of set:\n");
 	scanf("%d",&n);
+	n1=pow(2,n);
+	int set[n],a[n1][n];
 	printf("enter the elements:\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&set[i]);
 	}
-	n1=pow(2,n);
-	num=1;
 	for(i=0;i<n1;i++)
 	{
+		num=i+1;
 		for(j=0;j<n;j++)
 		{
 			rem=num%2;
@@ -25,7 +26,6 @@ int main(void)
 			}
 		}
 		printf("\n");
-		num=i+2;
 	}
 	return 0;
 }
